51f2b957b9a35483338d86e4224a5a35a876dca6
suhyun <suhyun5701@gmail.com>
2023-07-30 23:25:01 +0900

    23.07.31_authorizationfilter

--- toyproject1/src/main/java/com/suhyun/toyproject1/config/SecurityConfig.java
+++ toyproject1/src/main/java/com/suhyun/toyproject1/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.suhyun.toyproject1.config.auth.JWTAuthorizationFilter;
 import com.suhyun.toyproject1.persistance.MemberRepository;
 
 @Configuration
--- toyproject1/src/main/java/com/suhyun/toyproject1/config/auth/JWTAuthorizationFilter.java
+++ toyproject1/src/main/java/com/suhyun/toyproject1/config/auth/JWTAuthorizationFilter.java
@@ -1,11 +1,22 @@
 package com.suhyun.toyproject1.config.auth;
 
+import java.io.IOException;
+import java.util.Optional;
+
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.suhyun.toyproject1.domain.Member;
 import com.suhyun.toyproject1.persistance.MemberRepository;
 
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -27,13 +38,31 @@ public class JWTAuthorizationFilter extends BasicAuthenticationFilter{
 
     // 필터링 매소드를 오버라이드
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
 
         String srcToken = request.getHeader("Authorization");
-        if(srcToken == null || !srcToken.startsWith("Bearer")) {
+        if(srcToken == null || !srcToken.startsWith("Bearer ")) {
             chain.doFilter(request, response);
             return;
         }
+        String jwtToken = srcToken.replace("Bearer ", "");
+        String username = JWT.require(Algorithm.HMAC256("com.suhyun.toyproject1.jwtkey")).build().verify(jwtToken).getClaim("username").asString();
+
+        // 토큰에서 얻은 username으로 DB를 검색해서 사용자를 찾고
+        Optional<Member> opt = memRepo.findById(username);
+        if(!opt.isPresent()) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        Member findMember = opt.get();
+        //DB에서 읽은 사용자 정보를 이용해서 UserDetails 타입의 객체를 만들어서
+        User user = new User(findMember.getUsername(), findMember.getPassword(), findMember.getAuthorities());
+        //Authentication 객체를 생성 : 사용자명과 권한 관리를 위한 정보를 입력(암호는 필요 없음)
+        Authentication auth = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+        // 시큐리티 세션에 등록한다.
+        SecurityContextHolder.getContext().setAuthentication(auth);
+        chain.doFilter(request, response);
 
     }
 
